500d345934f644e6a17c43d192d7d35b601b47f8
Andrey Vetrov <vetrik@me.com>
2023-02-05 20:22:08 +0300

    Add new viewController with swiftUI preview and gameLogo on top

---
+++ HWTBAM/Controllers/SuccessTableView.swift
@@ -0,0 +1,72 @@
+//
+//  SuccessTableView.swift
+//  HWTBAM
+//
+//  Created by Андрей Ветров on 05.02.2023.
+//
+
+import UIKit
+import SwiftUI
+
+struct ViewControllerPreview: UIViewControllerRepresentable {
+    let viewControllerGenerator: () -> UIViewController
+
+    init(viewControllerGenerator: @escaping () -> UIViewController) {
+        self.viewControllerGenerator = viewControllerGenerator
+    }
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        viewControllerGenerator()
+    }
+
+    func updateUIViewController(_ UIViewController: UIViewControllerType, context: Context) {
+
+    }
+}
+
+class SuccessTableView: UIViewController {
+    
+    let gameLogoView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "gameLogo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+        setupViews()
+        setConstraints()
+    }
+    
+    func setupViews() {
+        view.addSubview(gameLogoView)
+    }
+
+
+}
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ViewControllerPreview {
+            SuccessTableView()
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+extension SuccessTableView {
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            gameLogoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            gameLogoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gameLogoView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.30),
+            gameLogoView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.30),
+            
+         ])
+    }
+
+    
+}
